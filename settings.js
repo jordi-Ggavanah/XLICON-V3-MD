b74797d71563d730669248608b00492edc0d8f58
jordi-Ggavanah <76637414+jordi-Ggavanah@users.noreply.github.com>
2024-03-28 13:23:42 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -50,10 +50,10 @@ global.location = process.env.GL_INFO || "Pakistan, Lahore, Sabzazar" //ur locat
 
 //new
 //________________________________________________________________________________________________________
-global.botname = process.env.BOT_NAME || 'XLICON-V3-MD' //ur bot name
+global.botname = process.env.BOT_NAME || 'Jordi Ggavanah' //ur bot name
 //________________________________________________________________________________________________________
-global.ownernumber = process.env.SUDO || '923184070915' //ur owner number
-//________________________________________________________________________________________________________
+global.ownernumber = process.env.SUDO || '256766165088' //ur owner number
+//_____________________________________________________________________________________________________
 global.ownername = process.env.SUDO_NAME || 'Salman Ahmad' //ur owner name
 //________________________________________________________________________________________________________
 global.websitex = process.env.YT_LINK || "https://youtube.com/@s4salmanyt"
